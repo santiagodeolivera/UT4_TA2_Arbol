03a08a20908a4f9fe0fdfef3623b550c862fd2c7
Santiago De Olivera <santiagodeolivera@gmail.com>
2022-04-28 10:42:03 -0300

    Iniciar tests

---
+++ src/test/java/grupo1/ut4_ta2/TArbolBBTest.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package grupo1.ut4_ta2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author santi
+ */
+public class TArbolBBTest {
+    
+    public TArbolBBTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+    
+    /**
+     * Test de búsqueda de elementos en un árbol binario.
+     * Tenemos un elemento que está en el árbol, verificar si está en el árbol.
+     */
+    @Test
+    public void testBuscar() {
+        
+    }
+
+    /**
+     * Test de iteración de elementos de un árbol binario en pre-orden.
+     * Comparar el resultado de la iteración con un resultado esperado.
+     */
+    @Test
+    public void testPreOrden() {
+        
+    }
+    
+    /**
+     * Test de iteración de elementos de un árbol binario en in-orden.
+     * Comparar el resultado de la iteración con un resultado esperado.
+     */
+    @Test
+    public void testInOrden() {
+        
+    }
+
+}
